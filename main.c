27a0c357608029f57c11164fe413613a99afb2ca
Hussien-Mostafa <73021151+Hussien-Mostafa@users.noreply.github.com>
2022-09-18 10:04:30 +0200

    Editedd

--- main.c
+++ main.c
@@ -3,28 +3,18 @@
 
 void ahmedEzzatFunction()
 {
-    char x = 72;
-    printf("%c",x);
-    x = 85;
-    printf("%c", x);
-}
+
 void aymanFunction()
 {
-    char x = 83;
-    printf("%c", x);
-    printf("%c", x);
+
 }
 void aishaFunction()
 {
-    char x = 73;
-    printf("%c", x);
-    x = 69;
-    printf("%c", x);
+
 }
 void mariamSolimanFunction()
 {
-    char x = 78;
-    printf("%c", x);
+
 }
 
 int main()
