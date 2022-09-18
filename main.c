b2dd517ee722f0cb4c72774ff40508eff2e4eada
Hussien-Mostafa <73021151+Hussien-Mostafa@users.noreply.github.com>
2022-09-18 09:27:13 +0200

    first-Commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ahmedEzzatFunction()
+{
+
+}
+void aymanFunction()
+{
+
+}
+void aishaFunction()
+{
+
+}
+void mariamSolimanFunction()
+{
+
+}
+
+int main()
+{
+    ahmedEzzatFunction();
+    aymanFunction();
+    aishaFunction();
+    mariamSolimanFunction();
+    return 0;
+}
+
